ad49ff4272507ca6333afd24d33344df32bc2ecd
ZarekChung <simen1127@gmail.com>
2017-12-16 13:58:41 +0800

    22 練習 Action Filter 的設計方式

---
+++ MVC5Course/ActionFilters/ShareDataAttrubute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.ActionFilters
+{
+	public class ShareDataAttrubute : ActionFilterAttribute
+	{
+		public override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			filterContext.Controller.ViewBag.Message = "Your application description page.";
+			base.OnActionExecuted(filterContext);
+		}
+	}
+}
--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.ActionFilters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,19 +9,20 @@ namespace MVC5Course.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+		[ShareDataAttrubute]
+		public ActionResult Index()
         {
             return View();
         }
-
+		[ShareDataAttrubute]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
-        public ActionResult Contact()
+		[ShareDataAttrubute]
+		public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
